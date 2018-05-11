db12c97c3bf4794ff357a9ecf0afa62a63d5bbde
syon <syon.xiii@gmail.com>
2018-05-12 00:44:43 +0900

    :cyclone: rename test

---
+++ test/scripts/rename.test.js
@@ -0,0 +1,25 @@
+const cyfs = require("../../")
+const yaml = require("js-yaml")
+
+test("rename regex", () => {
+  const order = yaml.safeLoad(`
+  select:
+    pattern: "test/dataset/week/*.log"
+  action:
+    do: rename
+    options:
+      regex: true
+      find: ^(F.*)
+      replace: PREMIUM-$1
+  `)
+  const expected = [
+    {
+      before: "test/dataset/week/Friday.log",
+      after: "test/dataset/week/PREMIUM-Friday.log",
+      renamed: true,
+    },
+  ]
+  const result = cyfs(order, { preview: true })
+  const renamed = result.list.filter(o => o.renamed)
+  expect(renamed).toEqual(expected)
+})
