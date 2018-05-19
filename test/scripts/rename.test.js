f29c289e61a18989aaddbdeb2e0be1451616d81c
syon <syon.xiii@gmail.com>
2018-05-20 02:00:13 +0900

    :shirt: exif test

--- test/scripts/rename.test.js
+++ test/scripts/rename.test.js
@@ -1,7 +1,7 @@
 const cyfs = require("../../")
 const yaml = require("js-yaml")
 
-test("rename regex", () => {
+test("rename regex", async () => {
   const order = yaml.safeLoad(`
   select:
     pattern: "test/dataset/week/*.log"
@@ -13,6 +13,6 @@ test("rename regex", () => {
       replace: PREMIUM-$1
   `)
   const expected = ["test/dataset/week/PREMIUM-Friday.log"]
-  const result = cyfs(order, { preview: true })
+  const result = await cyfs(order, { preview: true })
   expect(result).toEqual(expected)
 })
--- test/scripts/select.test.js
+++ test/scripts/select.test.js
@@ -3,7 +3,7 @@ const yaml = require("js-yaml")
 const shell = require("shelljs")
 const exec = require("child_process").execSync
 
-test("select name contain", () => {
+test("select name contain", async () => {
   const order = yaml.safeLoad(`
   select:
     pattern: "test/dataset/colors/**"
@@ -17,10 +17,10 @@ test("select name contain", () => {
     "test/dataset/colors/blue.txt",
     "test/dataset/colors/green.txt",
   ]
-  expect(cyfs(order)).toEqual(expected)
+  expect(await cyfs(order)).toEqual(expected)
 })
 
-test("select regex", () => {
+test("select regex", async () => {
   const order = yaml.safeLoad(`
   select:
     pattern: "test/dataset/colors/**"
@@ -31,10 +31,10 @@ test("select regex", () => {
           flags: i
   `)
   const expected = ["test/dataset/colors/red.txt"]
-  expect(cyfs(order)).toEqual(expected)
+  expect(await cyfs(order)).toEqual(expected)
 })
 
-test("select size min/max", () => {
+test("select size min/max", async () => {
   const order = yaml.safeLoad(`
   select:
     pattern: "test/dataset/**"
@@ -47,10 +47,10 @@ test("select size min/max", () => {
     "test/dataset/coffee/Grande.coffee",
     "test/dataset/coffee/Tall.coffee",
   ]
-  expect(cyfs(order)).toEqual(expected)
+  expect(await cyfs(order)).toEqual(expected)
 })
 
-test("select date after/before", () => {
+test("select date after/before", async () => {
   // Run on Mac
   exec('touch -mt 201805041259.59 "test/dataset/holidays/Greenery Day.h"')
   exec('touch -mt 201805051259.59 "test/dataset/holidays/Children\'s Day.h"')
@@ -70,10 +70,24 @@ test("select date after/before", () => {
     "test/dataset/holidays/Children's Day.h",
     "test/dataset/holidays/Marine Day.h",
   ]
-  expect(cyfs(order)).toEqual(expected)
+  expect(await cyfs(order)).toEqual(expected)
 })
 
-test("select preset emptydir", () => {
+test("select date after/before exif", async () => {
+  const order = yaml.safeLoad(`
+  select:
+    pattern: "test/dataset/photos/**/*"
+    include:
+      date:
+        exif:
+          after: "2013-01-04"
+          before: "2013-01-06"
+  `)
+  const expected = ["test/dataset/photos/drip.jpg"]
+  expect(await cyfs(order)).toEqual(expected)
+})
+
+test("select preset emptydir", async () => {
   shell.mkdir("-p", "test/dataset/blankdir")
   const order = yaml.safeLoad(`
   select:
@@ -82,5 +96,5 @@ test("select preset emptydir", () => {
       preset: emptydir
   `)
   const expected = ["test/dataset/blankdir"]
-  expect(cyfs(order)).toEqual(expected)
+  expect(await cyfs(order)).toEqual(expected)
 })
