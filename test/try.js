122c20bd6705fe8e4643ef9b58bda62555ae81fe
syon <syon.xiii@gmail.com>
2018-08-18 21:30:01 +0900

    try

--- test/try.js
+++ test/try.js
@@ -1,20 +1,33 @@
+const debug = require("debug")
 const yaml = require("js-yaml") // eslint-disable-line
 const cyfs = require("..")
 
+const vw = debug("cyfs:view")
+debug.enable("cyfs:*")
+
 const doc = yaml.safeLoad(`
 select:
-  pattern: "test/dataset/photos/**"
+  pattern: "/Users/syon/Downloads/20180818WK/**/*.*"
   include:
     date:
-      mode: exif
-      after: "2013-01-04"
-      before: "2013-01-06"
+      mode: modify
+      after: "2018-01-01"
+      before: "2018-12-31"
+action:
+  do: "rename"
+  args:
+    timestamp:
+      preferExif: true
+      format: "YYYY-MM-DDTHHmmss"
+      only: true
+    filesize: true
 `)
 
 /* eslint-disable */
-console.log(doc)
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-cyfs(doc, { preview: true, force: false }).then(res => {
-  console.log(res)
-  console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-})
+vw(doc)
+;(async () => {
+  vw(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  const res = await cyfs(doc, { preview: 1, force: false })
+  res.forEach(fp => vw(fp))
+  vw("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+})()
