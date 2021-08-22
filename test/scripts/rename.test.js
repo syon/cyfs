078bdcc53bd35018aa8bd6e6780b5d03693e53bb
syon <syon.xiii@gmail.com>
2021-08-22 22:03:26 +0900

    :recycle: add rename test for exif

--- test/scripts/rename.test.js
+++ test/scripts/rename.test.js
@@ -16,3 +16,24 @@ test("rename regex", async () => {
   const result = await cyfs(order, { preview: true })
   expect(result).toEqual(expected)
 })
+
+test("rename with timestamp", async () => {
+  const order = yaml.load(`
+  select:
+    pattern: "test/dataset/exif/**"
+  action:
+    do: rename
+    args:
+      timestamp:
+        preferExif: true
+        format: "YYYY-MM-DDTHHmmss"
+        only: true
+      filesize: true
+  `)
+  const expected = {
+    from: "test/dataset/exif/drip.jpg",
+    to: "test\\dataset\\exif\\2013-01-05T112157(10913B).jpg",
+  }
+  const result = await cyfs(order, { preview: true })
+  expect(result[0]).toEqual(expected)
+})
